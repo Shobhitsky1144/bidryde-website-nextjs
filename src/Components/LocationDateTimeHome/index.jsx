9b5411d77c6c0de34b61cdd8ba73f0a5174c4c24
Shobhitsky1144 <shobhitsky1144@gmail.com>
2025-07-26 14:06:38 +0530

    ff

--- src/Components/LocationDateTimeHome/index.jsx
+++ src/Components/LocationDateTimeHome/index.jsx
@@ -731,7 +731,7 @@ const LocationDateTimePickerHome = ({ actionHandler }) => {
               //   {window.innerWidth < 768 ? "Get Your Car" : "Search Cars"}
               // </span>
               <span className="responsive-text">
-              {isMobile ? "Get Your Car" : "Search Cars"}
+              {isMobile ? "Get Your Car" : "Search Carsddbb"}
             </span>
             
             )
