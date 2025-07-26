b0adb6e840af7d98c8afa01b7b5d29732e3dc70c
Shobhitsky1144 <shobhitsky1144@gmail.com>
2025-07-26 13:18:44 +0530

    b

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Home from "./home";
 import "./page.module.scss";
 import "./globals.scss";
 import "react-toastify/dist/ReactToastify.css";
-
+;
 export default function Main() {
   return (
     <>
