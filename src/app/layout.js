aad0d3d8ccca19d67c23a8ed8aa7b1f1f74692cf
Shobhitsky1144 <shobhitsky1144@gmail.com>
2025-08-04 14:13:35 +0530

    s

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,23 @@
-import { Inter } from 'next/font/google'
+// app/layout.js
 import './globals.scss'
+import { Inter, Archivo, Archivo_Black, Krona_One } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
+const archivo = Archivo({ subsets: ['latin'], variable: '--font-archivo' })
+const archivoBlack = Archivo_Black({ subsets: ['latin'], variable: '--font-archivo-black' })
+const krona = Krona_One({ subsets: ['latin'], variable: '--font-krona' })
 
 export const metadata = {
   title: 'BidRyde',
-  description: 'Reservce your ride',
+  description: 'Reserve your ride',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.variable} ${archivo.variable} ${archivoBlack.variable} ${krona.variable}`}>
+        {children}
+      </body>
     </html>
   )
 }
